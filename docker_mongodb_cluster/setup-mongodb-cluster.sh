eca62ae9ddd8af85c20e3311cca794d288e38692
Bogdan Apetrei <bogdanapetrei85@yahoo.com>
2014-11-02 11:03:43 +0200

    work in progress

--- docker_mongodb_cluster/setup-mongodb-cluster.sh
+++ docker_mongodb_cluster/setup-mongodb-cluster.sh
@@ -1,160 +1,191 @@
 #!/bin/bash
 
-ip_rs1_srv1=""
-ip_rs1_srv2=""
-ip_rs1_srv3=""
-ip_cfg1=""
-ip_mongos1=""
-default_port="27017"
+#https://github.com/jacksoncage/mongo-docker/blob/master/docker_mongodb_cluster/start_cluster.sh
 
-#clean
+# Docker interface ip
+DOCKERIP="172.17.42.1"
+
+#sudo docker ps -a -q | sudo xargs docker stop | sudo xargs docker rm
+
 echo "********************************************Cleaning the environment"
-sudo docker ps -a -q | sudo xargs docker stop | sudo xargs docker rm
-sudo rm -r \
- "`pwd`/mongo-persisted-data/mongo_srv1" \
- "`pwd`/mongo-persisted-data/mongo_srv2" \
- "`pwd`/mongo-persisted-data/mongo_srv3" \
- "`pwd`/mongo-persisted-data/mongo_cfg1" \
- "`pwd`/mongo-persisted-data/mongo_mongos1"
+containers=(skydns skydock rs1_srv1 rs1_srv2 rs1_srv3 cfg1 mongos1 rs1_srv2_bis)
+for c in ${containers[@]}; do
+	docker stop ${c} 	> /dev/null 2>&1
+	docker rm ${c} 		> /dev/null 2>&1
+	rm -r mongo-persisted-data/${c}
+done
 
+echo "***********************Restart docker to avoid some skydns problems."
+service docker.io restart
+sleep 5
 
-#create docker images
-echo "********************************************create docker images"
-sudo docker build -t dev0/mongodb mongod
-sudo docker build -t dev0/mongos mongos
+echo "***********************************************Setup skydns/skydock."
+docker run -d -p ${DOCKERIP}:53:53/udp --name skydns crosbymichael/skydns -nameserver 8.8.8.8:53 -domain docker
+docker run -d -v /var/run/docker.sock:/docker.sock --name skydock crosbymichael/skydock -ttl 30 -environment dev -s /docker.sock -domain docker -name skydns
 
-#create a replica set
-echo "********************************************create a replica set"
+sleep 2
+
+echo "*******************************************Create mongodocker images"
+docker build -t dev0/mongodb mongod
+docker build -t dev0/mongos mongos
+
+echo "************************************************Create a replica set"
 for i in {1..3}
 do
-    data_dir="`pwd`/mongo-persisted-data/mongo_srv${i}"
-    mkdir -p ${data_dir}
-
     container_name="rs1_srv${i}"
 
-    sudo docker run \
+    data_dir="`pwd`/mongo-persisted-data/${container_name}"
+    mkdir -p ${data_dir}
+
+    docker run \
+        --dns ${DOCKERIP}  \
         --name ${container_name} \
         -v ${data_dir}:/data/mongodb \
         -P -i -d dev0/mongodb \
         --replSet rs1 --dbpath /data/mongodb --noprealloc --smallfiles --profile=0 --slowms=-1
 
-    IP=$(sudo docker inspect ${container_name} | grep IPAddress | cut -d '"' -f 4)
-    echo "IP for replica server ${i} is ${IP}"
-    case ${i} in
-        1) ip_rs1_srv1=$IP
-           ;;
-        2) ip_rs1_srv2=$IP
-           ;;
-        3) ip_rs1_srv3=$IP
-           ;;
-    esac
+    sleep 2
 done
 
 sleep 5
 
-mongo --port 49153 <<EOF
+mongo --port $(docker port rs1_srv1 27017|cut -d ":" -f2) <<EOF
     config = { _id: "rs1", members:[
-              { _id : 0, host : "${ip_rs1_srv1}:${default_port}" },
-              { _id : 1, host : "${ip_rs1_srv2}:${default_port}" },
-              { _id : 2, host : "${ip_rs1_srv3}:${default_port}" }]
+              { _id : 0, host : "rs1_srv1.mongodb.dev.docker:27017" },
+              { _id : 1, host : "rs1_srv2.mongodb.dev.docker:27017" },
+              { _id : 2, host : "rs1_srv3.mongodb.dev.docker:27017" }]
              };
     rs.initiate(config)
 EOF
 
 sleep 20
 
-mongo --port 49153 <<EOF
+mongo --port $(docker port rs1_srv1 27017|cut -d ":" -f2) <<EOF
     rs.status()
 EOF
+
+sleep 2
 
 #create some config servers
 echo "********************************************create some config servers"
 
-sudo docker run \
+docker run \
+    --dns ${DOCKERIP} \
     --name cfg1 \
-    -v "`pwd`/mongo-persisted-data/mongo_cfg1":/data/mongodb \
+    -v "`pwd`/mongo-persisted-data/cfg1":/data/mongodb \
     -P -i -d dev0/mongodb \
     --configsvr --dbpath /data/mongodb --noprealloc --smallfiles --profile=0  --slowms=-1 --port 27017
 
-ip_cfg1=$(sudo docker inspect cfg1 | grep IPAddress | cut -d '"' -f 4)
-echo "IP for config server is ${ip_cfg1}"
+sleep 10
+
+echo "************************************************create mongod router"
+
+docker run \
+    --dns ${DOCKERIP} \
+    --name mongos1 \
+    -P -i -d dev0/mongos \
+    --configdb "cfg1.mongodb.dev.docker:27017" \
+    --port 27017
 
 sleep 10
 
-##create mongod router
-echo "********************************************create mongod router"
-
-sudo docker run \
-    --name mongos1 \
-    -P -i -d dev0/mongos \
-    --configdb "${ip_cfg1}:${default_port}" \
-    --port 27017
-
-ip_mongos1=$(sudo docker inspect mongos1 | grep IPAddress | cut -d '"' -f 4)
-echo "IP for mongod router is ${ip_mongos1}"
-
-sleep 5
-
-mongo "${ip_mongos1}:${default_port}" <<EOF
-    sh.addShard("rs1/${ip_rs1_srv1}:${default_port}")
+mongo --port $(docker port mongos1 27017|cut -d ":" -f2) <<EOF
+    sh.addShard("rs1/rs1_srv1.mongodb.dev.docker:27017")
     sh.status()
 EOF
 
 sleep 5
 
-#sudo docker logs rs1_srv1
-#sudo docker logs rs1_srv2
-#sudo docker logs rs1_srv3
+echo "***********************************************Populate initial data"
 
-#sudo docker logs cfg1
-#sudo docker logs mongos1
+mongo --port $(docker port mongos1 27017|cut -d ":" -f2) < js/populateDatabase.js
 
-echo "********************************************populate data"
+echo "**********************************************Check data replication"
 
-mongo "${ip_mongos1}:${default_port}" <<EOF
-
-use test_database
-
-for(var i=0; i< 1000; i++) {
-    db.test_collections.save({
-       nr: i,
-       createdDtm: new Date().getTime(),
-       randomValue: Math.random()
-    });
-}
-
-EOF
-
-echo "********************************************check data data"
-
-mongo "${ip_rs1_srv1}:${default_port}" <<EOF
-
-use test_database
-rs.slaveOk();
-var count = db.test_collections.count()
-print ("Number of documents on ${ip_rs1_srv1}: " + count)
-
-EOF
+mongo --port $(docker port rs1_srv1 27017|cut -d ":" -f2) < js/checkNumberOfDocuments.js
 
 sleep 5
 
-mongo "${ip_rs1_srv2}:${default_port}" <<EOF
-
-use test_database
-rs.slaveOk();
-var count = db.test_collections.count()
-print ("Number of documents on ${ip_rs1_srv2}: " + count)
-
-EOF
+mongo --port $(docker port rs1_srv2 27017|cut -d ":" -f2) < js/checkNumberOfDocuments.js
 
 sleep 5
 
-mongo "${ip_rs1_srv3}:${default_port}" <<EOF
+mongo --port $(docker port rs1_srv3 27017|cut -d ":" -f2) < js/checkNumberOfDocuments.js
 
-use test_database
-rs.slaveOk();
-var count = db.test_collections.count()
-print ("Number of documents on ${ip_rs1_srv3}: " + count)
+sleep 5
 
+
+echo "*******************************Create index on secondary replication"
+echo "**********************************************Put down one secondary"
+
+docker stop -t=10 rs1_srv2
+
+sleep 5
+
+mongo --port $(docker port rs1_srv1 27017|cut -d ":" -f2) <<EOF
+    rs.status()
 EOF
 
+echo "***********************************Create new secondary on same data"
+
+docker run \
+     --dns ${DOCKERIP}  \
+     --name rs1_srv2_bis \
+     -v "`pwd`/mongo-persisted-data/rs1_srv2":/data/mongodb \
+     -P -i -d dev0/mongodb \
+     --dbpath /data/mongodb --noprealloc --smallfiles --profile=0 --slowms=-1
+
+sleep 5
+
+docker ps -a
+
+mongo --port $(docker port rs1_srv2_bis 27017|cut -d ":" -f2) < js/addIndexOnlyOnSecondary.js
+
+sleep 5
+
+echo "***********************************************Restart old secondary"
+
+docker stop -t=10 rs1_srv2_bis
+
+docker start rs1_srv2
+
+sleep 5
+
+mongo --port $(docker port rs1_srv1 27017|cut -d ":" -f2) <<EOF
+    rs.status()
+EOF
+
+echo "*******************************************************Execute query"
+
+mongo --port $(docker port rs1_srv2 27017|cut -d ":" -f2) <<EOF
+    rs.slaveOk();
+    use test_database;
+
+    printjson(db.test_collections.find({randomValue: {\$gt: 0}, createdDtm: {\$gt: 0}}).limit(1).explain(true));
+EOF
+
+echo "***********************************************Populate initial data"
+
+mongo --port $(docker port mongos1 27017|cut -d ":" -f2) < js/populateDatabase.js
+
+echo "**********************************************Check data replication"
+
+mongo --port $(docker port rs1_srv1 27017|cut -d ":" -f2) < js/checkNumberOfDocuments.js
+
+sleep 5
+
+mongo --port $(docker port rs1_srv2 27017|cut -d ":" -f2) < js/checkNumberOfDocuments.js
+
+sleep 5
+
+mongo --port $(docker port rs1_srv3 27017|cut -d ":" -f2) < js/checkNumberOfDocuments.js
+
+
+##
+###sudo docker logs rs1_srv1
+###sudo docker logs rs1_srv2
+###sudo docker logs rs1_srv3
+##
+###sudo docker logs cfg1
+###sudo docker logs mongos1
+##
